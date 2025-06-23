47feeddbc0938ffb18482ad0a464fb8d6843591d
dardo.camano <dardo.camano@eest3necochea.edu.ar>
2025-06-23 19:53:42 -0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,3 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { MisLibrosComponent } from './mis-libros/mis-libros.component';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'mis-libros', component: MisLibrosComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routes: Routes = [];
